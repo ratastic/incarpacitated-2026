65297b6ae16367a357c3ad45ad1ca60e92811453
Drinomino <pamelamckeown2004@gmail.com>
2026-01-31 17:05:13 -0500

    Revert "Arm strech, sprite change, wallet scene change"
    This reverts commit a59145f64f80e183c61f94922463e0ae3b9c5ce5.

--- ggj-mask-2026/Assets/scripts/BagSerchScripts/Arm.cs
+++ ggj-mask-2026/Assets/scripts/BagSerchScripts/Arm.cs
@@ -2,19 +2,18 @@ using UnityEngine;
 
 public class Arm : MonoBehaviour
 {
-    public SpriteRenderer armEnd;
-    Vector3 startPoint;
+    private Vector3 offset;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        startPoint = transform.parent.position;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
 
         MovingHand();
     }
@@ -22,18 +21,10 @@ public class Arm : MonoBehaviour
     public void MovingHand()
     {
         Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         newPosition.z = 0;
 
         transform.position = newPosition;
-
-        //turns hand and arm direction
-        Vector3 direction = newPosition - startPoint;
-        transform.up = direction;
-
-        //arm streching and connecting to hand and base
-        float dist = Vector2.Distance(startPoint, newPosition);
-        armEnd.size = new Vector2(dist, armEnd.size.y);
     }
 }
