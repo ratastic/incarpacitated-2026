d006ec0212cb6286b88ed3c75e29e7c25b038a42
Drinomino <pamelamckeown2004@gmail.com>
2026-01-31 17:33:28 -0500

    hand moving and spitre change
    for the searching bag scene

--- ggj-mask-2026/Assets/scripts/BagSerchScripts/Arm.cs
+++ ggj-mask-2026/Assets/scripts/BagSerchScripts/Arm.cs
@@ -2,18 +2,18 @@ using UnityEngine;
 
 public class Arm : MonoBehaviour
 {
-    private Vector3 offset;
 
+    Vector3 startPoint;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        startPoint = transform.parent.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+       // transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
 
         MovingHand();
     }
@@ -21,10 +21,13 @@ public class Arm : MonoBehaviour
     public void MovingHand()
     {
         Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         newPosition.z = 0;
 
         transform.position = newPosition;
+
+        Vector3 direction = newPosition - startPoint;
+        transform.up = direction;
     }
 }
